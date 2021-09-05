87a862f091a2604dbddb6ce02d309a2c98f9cf9a
huibosa_arch <huibosa@gmal.com>
2021-09-05 16:41:32 +0800

    change backgroud color to everforest

--- config.h
+++ config.h
@@ -120,7 +120,7 @@ static const char *colorname[] = {
   [15] = "#ffffff", /* white   */
 
   /* special colors */
-  [256] = "#282a36", /* background */
+  [256] = "#2f383e", /* background */
   [257] = "#f8f8f2", /* foreground */
 };
 
