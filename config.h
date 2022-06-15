ba9f79a5256a7686e43c3bce2c5ab53587c53b06
huibosa_arch <huibosa@gmail.com>
2022-06-15 18:39:49 +0800

    feat(font): Change font to JetBrains Mono

--- config.h
+++ config.h
@@ -5,7 +5,8 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "Source Code Pro:pixelsize=21:antialias=true:autohint=true";
+static char *font = "Source Code Pro:pixelsize=20:antialias=true:autohint=true";
+// static char *font = "JetBrains Mono:pixelsize=19:antialias=true:autohint=true";
 static int borderpx = 2;
 
 /*
@@ -94,7 +95,8 @@ char *termname = "st-256color";
 unsigned int tabspaces = 8;
 
 /* bg opacity */
-float alpha = 0.8;
+// float alpha = 0.8;
+float alpha = 1.0;
 
 /* Terminal colors (16 first used in escape sequence) */
 static const char *colorname[] = {
