a347f81816c27204027b824ebe3c02e1df5f4242
huibosa_arch <huibosa@gmail.com>
2022-06-26 07:40:34 +0800

    feat(ligatures): Add support to ligatures

--- config.h
+++ config.h
@@ -5,8 +5,8 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "Source Code Pro:pixelsize=20:antialias=true:autohint=true";
-// static char *font = "JetBrains Mono:pixelsize=19:antialias=true:autohint=true";
+// static char *font = "Source Code Pro:pixelsize=20:antialias=true:autohint=true";
+static char *font = "JetBrains Mono:pixelsize=20:antialias=true:autohint=true";
 static int borderpx = 2;
 
 /*
